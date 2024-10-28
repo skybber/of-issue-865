f786464194873e3ef0c0a76542faa5343875f8cf
skybber <lada.dvorak7@gmail.com>
2024-10-28 14:13:07 +0100

    Of-issue-86

---
+++ src/main/java/org/hotswap/agent/example/deltaspike/appl/UserController.java
@@ -0,0 +1,18 @@
+package org.hotswap.agent.example.deltaspike.appl;
+
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+import org.omnifaces.cdi.ViewScoped;
+
+@Named
+@ViewScoped
+public class UserController implements Serializable {
+
+    private static final long serialVersionUID = -4538115105092134763L;
+
+    public void throwException() throws Exception
+		{
+        throw new Exception("Exception!");
+    }
+}
